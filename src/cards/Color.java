f5fac1cf2fbb8dfaf84cd16fefa1a5ba521d66c5
Thiago Lages de Alencar <thiagola92@gmail.com>
2017-03-31 17:08:42 -0300

    Reset
    Começando do zero

--- src/cards/Color.java
+++
@@ -1,8 +0,0 @@
-package cards;
-
-public enum Color {
-	NO_COLOR,
-	BLUE,
-	RED,
-	YELLOW;
-}
--- src/others/run.java
+++
@@ -1,9 +0,0 @@
-package others;
-
-public class run {
-
-	public static void main(String[] args) {
-		new Controller();
-	}
-
-}
